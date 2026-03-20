edead6dc5abf8d600f4974d27aafcafc5eb4a6e0
Abhishek Anand <abhishekanand@Abhisheks-MacBook-Air.local>
2026-03-20 14:00:56 +0530

    Add employee model validations (TDD)

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Employee, type: :model do
     employee = Employee.new(full_name: nil)
     expect(employee).not_to be_valid
   end
+  it "is invalid without job_title" do
+  employee = Employee.new(job_title: nil)
+  expect(employee).not_to be_valid
+end
+
+it "is invalid without country" do
+  employee = Employee.new(country: nil)
+  expect(employee).not_to be_valid
+end
+
+it "is invalid without salary" do
+  employee = Employee.new(salary: nil)
+  expect(employee).not_to be_valid
+end
+
+it "is invalid with salary <= 0" do
+  employee = Employee.new(salary: 0)
+  expect(employee).not_to be_valid
+end
 end
